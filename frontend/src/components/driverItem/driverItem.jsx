a97b395ff8ecc1edf50427ff58aff36b8092fe9a
Mister-718 <59221853+Mister-718@users.noreply.github.com>
2022-10-01 23:54:52 -0400

    surprise surprise

--- frontend/src/components/driverItem/driverItem.jsx
+++ frontend/src/components/driverItem/driverItem.jsx
@@ -12,7 +12,7 @@ export const DriverItem = () => {
                 <span id="to"><b>To: </b>New York City</span>
                 <span id="capacity"><b>Capacity: </b>3</span>
                 <span id="when"><b>When: </b>2.00 PM 10/01/2022</span>
-                <span id="contacts"><b>Contact driver:</b> 610-000-00-00, abc226@lehigh.edu</span>
+                <span id="contacts"><b>Contact driver:</b> 586-928-3251, dac323@lehigh.edu</span>
                 <div className="item-wrapper">
                     <div className="img-holder">
                         <img src="https://media-exp1.licdn.com/dms/image/C4E03AQExMd4ko1GwNQ/profile-displayphoto-shrink_800_800/0/1647900004636?e=2147483647&v=beta&t=t-yr4G5TfmjyGV8ocUwOrEjZL4eTwu_VzTkwn7xXFh8"></img>
